2c7f8dd41e2e497a29dfa41147cb423815a1a73b
Jabr0ski <51535527+Jabr0ski@users.noreply.github.com>
2020-12-29 16:32:42 +0800

    Smaller pics available locally
    Now pulls images from local files rather than downloading them everytime, speeding up loading hopefully.
    
    Shadows fixed.
    
    Extra text box added for details such as series title, year etc.
    
    Formatted better too.

--- myscript.js
+++ myscript.js
@@ -32,11 +32,13 @@ function changeText() {
     currCard = selectCard();
     document.getElementById("fq").innerHTML = currCard.quote;
     document.getElementById("fa").innerHTML = currCard.authour;
+    document.getElementById("fe").innerHTML = currCard.extra;
     document.getElementById("fi").src = currCard.background;
   } else {
     currCard = selectCard();
     document.getElementById("bq").innerHTML = currCard.quote;
     document.getElementById("ba").innerHTML = currCard.authour;
+    document.getElementById("be").innerHTML = currCard.extra;
     document.getElementById("bi").src = currCard.background;
   }
 }
@@ -46,21 +48,22 @@ function loadCards() {
   let q1 = new qCard("It is important to draw wisdom from \
   different places. If you take it from only one place, it \
   becomes rigid and stale.", "Uncle Iroh", "Avatar: The Last \
-  Airbender", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1b3bf719-8603-4211-a054-1e161ce09a89/dbi1458-63dec81f-fd88-491a-a108-08642109c6f6.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvMWIzYmY3MTktODYwMy00MjExLWEwNTQtMWUxNjFjZTA5YTg5XC9kYmkxNDU4LTYzZGVjODFmLWZkODgtNDkxYS1hMTA4LTA4NjQyMTA5YzZmNi5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.pANG4Qmw8NyMRpOVJomIkSOsvSEpfSD4jXsGpb6Jms4");
+  Airbender", "images/orange_valley.jpg");
   
   let q2 = new qCard("I see now that the circumstances of one's \
   birth are irrelevant. It is what you do with the gift of life \
   that determines who you are.", "Mewtwo", "Pokemon: The First Movie",
-  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/4220d224-afd0-40fb-bbf8-7fbb07bbbe18/dclmad6-a3273c41-6f48-48ca-9d45-621a942d173b.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvNDIyMGQyMjQtYWZkMC00MGZiLWJiZjgtN2ZiYjA3YmJiZTE4XC9kY2xtYWQ2LWEzMjczYzQxLTZmNDgtNDhjYS05ZDQ1LTYyMWE5NDJkMTczYi5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.p8rH9iXhu306YTS8HpZNEWfydNn3ueVCoew5GeSp_T8");
+  "images/blue_lighthouse.jpg");
+  // "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/4220d224-afd0-40fb-bbf8-7fbb07bbbe18/dclmad6-a3273c41-6f48-48ca-9d45-621a942d173b.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvNDIyMGQyMjQtYWZkMC00MGZiLWJiZjgtN2ZiYjA3YmJiZTE4XC9kY2xtYWQ2LWEzMjczYzQxLTZmNDgtNDhjYS05ZDQ1LTYyMWE5NDJkMTczYi5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.p8rH9iXhu306YTS8HpZNEWfydNn3ueVCoew5GeSp_T8");
 
   let q3 = new qCard("You should enjoy the little detours to the \
   fullest, because that's where you will find things more \
   important than what you want.", "Ging Freecss", "Hunter X Hunter",
-  "https://i.redd.it/b3esnz5ra34y.jpg");
+  "images/green_forest.jpg");
 
   let q4 = new qCard("Hate is a place where a man who can't \
   stand sadness goes.", "Godo", "Berserk", 
-  "https://i.redd.it/b3esnz5ra34y.jpg" )
+  "images/purple_mountain.jpg" )
 
   qCards = [q1, q2, q3, q4];
 }
